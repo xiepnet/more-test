19fc1e5e5020d177538966f1acae6ae2a04b3732
xiepnet <xiepnet@gmail.com>
2015-05-22 00:23:54 +0800

    delete

--- fe-guide/js/guide.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$(".menu li").bind("click", function(){
-		$(this).addClass("current").siblings().removeClass("current");
-		var liindex = $(this).index();
-		$(".main").eq(liindex).show().siblings(".main").hide();
-	})
-})
